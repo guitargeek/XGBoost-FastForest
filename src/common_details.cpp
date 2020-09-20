12c8b56bcd27c49b1802d5a54683958b3e0f7213
Jonas Rembser <jonas.rembser@cern.ch>
2020-09-20 14:02:13 +0200

    update license year to 2020

--- src/common_details.cpp
+++ src/common_details.cpp
@@ -2,7 +2,7 @@
 
 MIT License
 
-Copyright (c) 2019 Jonas Rembser
+Copyright (c) 2020 Jonas Rembser
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
