a690ee7443a925cedf5cdfd5152120168efc4d4a
Jonas Rembser <jonas.rembser@cern.ch>
2025-09-01 00:24:58 +0200

    Update copyright year to 2025

--- src/fastforest_functions.cpp
+++ src/fastforest_functions.cpp
@@ -2,7 +2,7 @@
 
 MIT License
 
-Copyright (c) 2020 Jonas Rembser
+Copyright (c) 2025 Jonas Rembser
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
